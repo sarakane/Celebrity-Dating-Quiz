2277f95cb903cf3bf89a88e34b743c05925e5988
Sara Kane <kane.sarak@gmail.com>
2020-06-04 13:36:49 -0700

    Add additional form fields and functionality to get a different result based on form answers
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,46 @@ $(document).ready(function() {
     event.preventDefault();
     
     const color = $("#color").val();
+    const iceCream = $("#icecream").val();
+    const animal = $("input:radio[name=animal]:checked").val();
+
     if(color === "red") {
+      if((iceCream === "chocolate" || iceCream === "vanilla") && animal === "dog") {
       $("#celebrity").text("Taylor Swift");
+      }
+      else if((iceCream === "chocolate" || iceCream === "vanilla") && animal === "elephant") {
+        $("#celebrity").text("Madonna");
+      }
+      else if(iceCream === "rockyroad") {
+        $("#celebrity").text("Beyoncé");
+      }
+      else {
+        $("#celebrity").text("Jennifer Aniston");
+        
+      }
       $("#results").show();
     }
+    
     else if (color === "yellow") {
-      $("#celebrity").text("Beyoncé");
+      if(iceCream === "chocolate" || iceCream === "vanilla") {
+        $("#celebrity").text("Oprah Winfrey");
+      }
+      else if(iceCream === "cookiesandcream") {
+        $("#celebrity").text("Robert Downey Jr");
+      }
+      else {
+        $("#celebrity").text("Britney Spears");
+        
+      }
       $("#results").show();
     }
     else if (color === "green") {
-      $("#celebrity").text("Oprah Winfrey");
+      if(iceCream === "chocolate" || iceCream === "cookiesandcream"){
+        $("#celebrity").text("George Clooney");
+      }
+      else {
+        $("#celebrity").text("Tom Hanks");
+      }
       $("#results").show();
     }
     else {
